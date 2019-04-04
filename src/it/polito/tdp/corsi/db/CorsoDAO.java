44fc87572095f5015c34ba047c958f603faee8fc
alessio-zamparelli <37191190+alessio-zamparelli@users.noreply.github.com>
2019-04-04 19:34:41 +0200

    done 1st point

--- src/it/polito/tdp/corsi/db/CorsoDAO.java
+++ src/it/polito/tdp/corsi/db/CorsoDAO.java
@@ -4,8 +4,12 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+
+import javax.management.RuntimeErrorException;
 
 import it.polito.tdp.corsi.model.Corso;
 
@@ -58,6 +62,36 @@ public class CorsoDAO {
 
 		} catch (SQLException e) {
 			e.printStackTrace();
+		}
+
+		return result;
+	}
+
+	public Map<Corso, Integer> getIscrittiCorsi(int periodo) {
+
+		String sql = "SELECT c.codins, c.nome, c.crediti, c.pd, COUNT(*) AS tot FROM corso AS C, iscrizione as i "
+				+ "WHERE c.codins = i.codins and c.pd=? GROUP BY c.codins, c.nome, c.crediti, c.pd";
+
+		Map<Corso, Integer> result = new HashMap<Corso, Integer>();
+
+		try {
+
+			Connection conn = ConnectDB.getConnection();
+			PreparedStatement st = conn.prepareStatement(sql);
+			st.setInt(1, periodo);
+			ResultSet rs = st.executeQuery();
+
+			while (rs.next())
+
+				result.put(
+						new Corso(rs.getString("codins"), rs.getInt("crediti"), rs.getString("nome"), rs.getInt("pd")),
+						rs.getInt("tot"));
+
+			conn.close();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
 		}
 
 		return result;
--- src/it/polito/tdp/corsi/model/GestoreCorsi.java
+++ src/it/polito/tdp/corsi/model/GestoreCorsi.java
@@ -1,16 +1,16 @@
 package it.polito.tdp.corsi.model;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import it.polito.tdp.corsi.db.CorsoDAO;
 import it.polito.tdp.corsi.model.Corso;
 
 public class GestoreCorsi {
 
-	public List<Corso> getCorsiByPeriodo(int periodo) {
+	CorsoDAO dao = new CorsoDAO();
 
-		CorsoDAO dao = new CorsoDAO();
+	public List<Corso> getCorsiByPeriodo(int periodo) {
 
 //		List<Corso> corsi = dao.listAll();
 //		List<Corso> result = new ArrayList<Corso>();
@@ -21,9 +21,13 @@ public class GestoreCorsi {
 //		}
 //
 //		return result;
-		
+
 		return dao.listCorsiByPD(periodo);
 
+	}
+
+	public Map<Corso, Integer> getIscrittiCorsi(int periodo) {
+		return dao.getIscrittiCorsi(periodo);
 	}
 
 }
